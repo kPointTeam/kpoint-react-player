1e7b838396e4b30eb74ad924d31aa9945714af80
Aditya Kumar Panda <dev@kpoint.com>
2022-04-05 12:24:13 +0530

    The player component's functionality has been updated to be class-based.

--- src/lib/components/KpointPlayer.js
+++ src/lib/components/KpointPlayer.js
@@ -1,85 +1,109 @@
-import React, { forwardRef, useImperativeHandle } from "react";
-const KpointPlayer = forwardRef((props, ref) => {
-  var script = document.getElementById("videofront-vega");
-  if (script != null) {
-    document.body.removeChild(script);
+import React, { Component } from "react";
+
+export class KpointPlayer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      kPlayer: null,
+    };
+    var script = document.getElementById("videofront-vega");
+    if (script != null) {
+      document.body.removeChild(script);
+    }
+    script = document.createElement("script");
+    script.src =
+      "https://assets.kpoint.com/orca/media/embed/videofront-vega.js";
+    script.id = "videofront-vega";
+    script.async = true;
+    document.body.appendChild(script);
+    const setKPlayer = (Player) => {
+      this.setState({ kPlayer: Player });
+    };
+    window.onkPointPlayerReady = function (player) {
+      setKPlayer(player);
+      if (props.onTimeupdate) {
+        player.addEventListener(player.events.timeUpdate, props.onTimeupdate);
+      }
+      if (props.onStateChange) {
+        player.addEventListener(
+          player.events.onStateChange,
+          props.onStateChange
+        );
+      }
+      if (props.onError) {
+        player.addEventListener(player.events.onError, props.onError);
+      }
+      if (props.onStart) {
+        player.addEventListener(player.events.started, props.onStart);
+      }
+      if (props.onload) {
+        player.addEventListener(player.events.loaded, props.onload);
+      }
+    };
   }
-  script = document.createElement("script");
-  script.src = "https://assets.kpoint.com/orca/media/embed/player-silk.js";
-  script.id = "videofront-vega";
-  script.async = true;
-  document.body.appendChild(script);
 
-  var kPlayer = null;
-  useImperativeHandle(ref, () => ({
-    getId() {
-      return kPlayer.id;
-    },
-    getInfo() {
-      return kPlayer.info;
-    },
-    playVideo() {
-      if (kPlayer.getPlayState() === kPlayer.playStates.UNSTARTED) {
-        kPlayer.startVideo();
-      } else {
-        kPlayer.playVideo();
-      }
-    },
-    pauseVideo() {
-      kPlayer.pauseVideo();
-    },
-    replayVideo() {
-      if (kPlayer.getPlayState() === kPlayer.playStates.ENDED) {
-        kPlayer.replayVideo();
-      }
-    },
-    getCurrentTime() {
-      return kPlayer.getCurrentTime();
-    },
-    getDuration() {
-      return kPlayer.getDuration();
-    },
-    getVolume() {
-      return kPlayer.getVolume();
-    },
-    seekTo(ms) {
-      kPlayer.seekTo(ms);
-    },
-    setVolume(vlevel) {
-      if (vlevel >= 0 && vlevel <= 1) {
-        kPlayer.setVolume(vlevel);
-      }
-    },
-  }));
-  window.onkPointPlayerReady = function (player) {
-    kPlayer = player;
-    if (props.onTimeupdate) {
-      player.addEventListener(player.events.timeUpdate, props.onTimeupdate);
+  getId() {
+    return this.state.kPlayer.id;
+  }
+  getInfo() {
+    return this.state.kPlayer.info;
+  }
+  playVideo() {
+    if (
+      this.state.kPlayer.getPlayState() ===
+      this.state.kPlayer.playStates.UNSTARTED
+    ) {
+      this.state.kPlayer.startVideo();
+    } else {
+      this.state.kPlayer.playVideo();
     }
-    if (props.onStateChange) {
-      player.addEventListener(player.events.onStateChange, props.onStateChange);
+  }
+  pauseVideo() {
+    this.state.kPlayer.pauseVideo();
+  }
+  replayVideo() {
+    if (
+      this.state.kPlayer.getPlayState() === this.state.kPlayer.playStates.ENDED
+    ) {
+      this.state.kPlayer.replayVideo();
     }
-    if (props.onError) {
-      player.addEventListener(player.events.onError, props.onError);
+  }
+  getCurrentTime() {
+    return this.state.kPlayer.getCurrentTime();
+  }
+  getDuration() {
+    return this.state.kPlayer.getDuration();
+  }
+  getVolume() {
+    return this.state.kPlayer.getVolume();
+  }
+  seekTo(ms) {
+    this.state.kPlayer.seekTo(ms);
+  }
+  setVolume(vlevel) {
+    if (vlevel >= 0 && vlevel <= 1) {
+      this.state.kPlayer.setVolume(vlevel);
+    } else {
+      alert("Volume level should be between 0 and 1");
     }
-    if (props.onload) {
-      player.addEventListener(player.events.loaded, props.onload);
-    }
+  }
+  playerStyle = {
+    width: this.props.width,
+    height: this.props.height,
   };
 
-  const playerStyle = {
-    width: props.width,
-    height: props.height,
-  };
-  return (
-    <div
-      data-video-host={props.domain}
-      data-kvideo-id={props.videoId}
-      style={playerStyle}
-      data-video-params={JSON.stringify(props.playerParams)}
-      data-widgets-config={JSON.stringify(props.widgetsConfig)}
-      data-player={props.playerId}
-    />
-  );
-});
+  render() {
+    return (
+      <div
+        data-video-host={this.props.domain}
+        data-kvideo-id={this.props.videoId}
+        style={this.playerStyle}
+        data-video-params={JSON.stringify(this.props.playerParams)}
+        data-widgets-config={JSON.stringify(this.props.widgetsConfig)}
+        data-player={this.props.playerId}
+      />
+    );
+  }
+}
+
 export default KpointPlayer;
